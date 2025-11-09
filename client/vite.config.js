caae756e4f718ebe7288199d0211ea62c96f33c3
Marzia Hashimi <marziajan130@gmail.com>
2025-11-09 23:00:41 +0500

    changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/language-ladders-ai/',
+  base: '/language-ladder-ai/', // 👈 must match your repo name exactly
   plugins: [react()],
 })
